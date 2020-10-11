6f1b1f463fd6adb35b6607958b1fca27d909013e
giscar <careli_2710@hotmail.com>
2020-10-11 04:33:00 -0500

    indicadores

--- script/001-MODELO.sql
+++ script/001-MODELO.sql
@@ -1,154 +1,163 @@
 if exists (select 1
    from sys.sysreferences r join sys.sysobjects o on (o.id = r.constid and o.type = 'F')
-   where r.fkeyid = object_id('ACCION') and o.name = 'FK_ACCION_REFERENCE_PLAN')
-alter table ACCION
+   where r.fkeyid = object_id('PRIVADO.ACCION') and o.name = 'FK_ACCION_REFERENCE_PLAN')
+alter table PRIVADO.ACCION
    drop constraint FK_ACCION_REFERENCE_PLAN
 go
 
 if exists (select 1
    from sys.sysreferences r join sys.sysobjects o on (o.id = r.constid and o.type = 'F')
-   where r.fkeyid = object_id('INDICADOR') and o.name = 'FK_INDICADO_REFERENCE_ACCION')
-alter table INDICADOR
+   where r.fkeyid = object_id('PRIVADO.INDICADOR') and o.name = 'FK_INDICADO_REFERENCE_ACCION')
+alter table PRIVADO.INDICADOR
    drop constraint FK_INDICADO_REFERENCE_ACCION
 go
 
 if exists (select 1
    from sys.sysreferences r join sys.sysobjects o on (o.id = r.constid and o.type = 'F')
-   where r.fkeyid = object_id('INDICADORENTIDAD') and o.name = 'FK_INDICADO_REFERENCE_ENTIDAD')
-alter table INDICADORENTIDAD
+   where r.fkeyid = object_id('PRIVADO.INDICADORENTIDAD') and o.name = 'FK_INDICADO_REFERENCE_ENTIDAD')
+alter table PRIVADO.INDICADORENTIDAD
    drop constraint FK_INDICADO_REFERENCE_ENTIDAD
 go
 
 if exists (select 1
    from sys.sysreferences r join sys.sysobjects o on (o.id = r.constid and o.type = 'F')
-   where r.fkeyid = object_id('INDICADORENTIDAD') and o.name = 'FK_INDICADO_REFERENCE_INDICADO')
-alter table INDICADORENTIDAD
+   where r.fkeyid = object_id('PRIVADO.INDICADORENTIDAD') and o.name = 'FK_INDICADO_REFERENCE_INDICADO')
+alter table PRIVADO.INDICADORENTIDAD
    drop constraint FK_INDICADO_REFERENCE_INDICADO
 go
 
 if exists (select 1
    from sys.sysreferences r join sys.sysobjects o on (o.id = r.constid and o.type = 'F')
-   where r.fkeyid = object_id('MENUROL') and o.name = 'FK_MENUROL_REFERENCE_MENU')
-alter table MENUROL
+   where r.fkeyid = object_id('PRIVADO.MENUROL') and o.name = 'FK_MENUROL_REFERENCE_MENU')
+alter table PRIVADO.MENUROL
    drop constraint FK_MENUROL_REFERENCE_MENU
 go
 
 if exists (select 1
    from sys.sysreferences r join sys.sysobjects o on (o.id = r.constid and o.type = 'F')
-   where r.fkeyid = object_id('MENUROL') and o.name = 'FK_MENUROL_REFERENCE_ROL')
-alter table MENUROL
+   where r.fkeyid = object_id('PRIVADO.MENUROL') and o.name = 'FK_MENUROL_REFERENCE_ROL')
+alter table PRIVADO.MENUROL
    drop constraint FK_MENUROL_REFERENCE_ROL
 go
 
 if exists (select 1
    from sys.sysreferences r join sys.sysobjects o on (o.id = r.constid and o.type = 'F')
-   where r.fkeyid = object_id('PERFILROL') and o.name = 'FK_PERFILRO_REFERENCE_PERFIL')
-alter table PERFILROL
+   where r.fkeyid = object_id('PRIVADO.PERFILROL') and o.name = 'FK_PERFILRO_REFERENCE_PERFIL')
+alter table PRIVADO.PERFILROL
    drop constraint FK_PERFILRO_REFERENCE_PERFIL
 go
 
 if exists (select 1
    from sys.sysreferences r join sys.sysobjects o on (o.id = r.constid and o.type = 'F')
-   where r.fkeyid = object_id('PERFILROL') and o.name = 'FK_PERFILRO_REFERENCE_ROL')
-alter table PERFILROL
+   where r.fkeyid = object_id('PRIVADO.PERFILROL') and o.name = 'FK_PERFILRO_REFERENCE_ROL')
+alter table PRIVADO.PERFILROL
    drop constraint FK_PERFILRO_REFERENCE_ROL
 go
 
 if exists (select 1
    from sys.sysreferences r join sys.sysobjects o on (o.id = r.constid and o.type = 'F')
-   where r.fkeyid = object_id('USUARIO') and o.name = 'FK_USUARIO_REFERENCE_ENTIDAD')
-alter table USUARIO
+   where r.fkeyid = object_id('PRIVADO.USUARIO') and o.name = 'FK_USUARIO_REFERENCE_ENTIDAD')
+alter table PRIVADO.USUARIO
    drop constraint FK_USUARIO_REFERENCE_ENTIDAD
 go
 
 if exists (select 1
    from sys.sysreferences r join sys.sysobjects o on (o.id = r.constid and o.type = 'F')
-   where r.fkeyid = object_id('USUARIO') and o.name = 'FK_USUARIO_REFERENCE_PERFIL')
-alter table USUARIO
+   where r.fkeyid = object_id('PRIVADO.USUARIO') and o.name = 'FK_USUARIO_REFERENCE_PERFIL')
+alter table PRIVADO.USUARIO
    drop constraint FK_USUARIO_REFERENCE_PERFIL
 go
 
 if exists (select 1
             from  sysobjects
-           where  id = object_id('ACCION')
+           where  id = object_id('PRIVADO.ACCION')
             and   type = 'U')
-   drop table ACCION
+   drop table PRIVADO.ACCION
 go
 
 if exists (select 1
             from  sysobjects
-           where  id = object_id('ENTIDAD')
+           where  id = object_id('PRIVADO.ENTIDAD')
             and   type = 'U')
-   drop table ENTIDAD
+   drop table PRIVADO.ENTIDAD
 go
 
 if exists (select 1
             from  sysobjects
-           where  id = object_id('INDICADOR')
+           where  id = object_id('PRIVADO.INDICADOR')
             and   type = 'U')
-   drop table INDICADOR
+   drop table PRIVADO.INDICADOR
 go
 
 if exists (select 1
             from  sysobjects
-           where  id = object_id('INDICADORENTIDAD')
+           where  id = object_id('PRIVADO.INDICADORENTIDAD')
             and   type = 'U')
-   drop table INDICADORENTIDAD
+   drop table PRIVADO.INDICADORENTIDAD
 go
 
 if exists (select 1
             from  sysobjects
-           where  id = object_id('MENU')
+           where  id = object_id('PRIVADO.MENU')
             and   type = 'U')
-   drop table MENU
+   drop table PRIVADO.MENU
 go
 
 if exists (select 1
             from  sysobjects
-           where  id = object_id('MENUROL')
+           where  id = object_id('PRIVADO.MENUROL')
             and   type = 'U')
-   drop table MENUROL
+   drop table PRIVADO.MENUROL
 go
 
 if exists (select 1
             from  sysobjects
-           where  id = object_id('PERFIL')
+           where  id = object_id('PRIVADO.PERFIL')
             and   type = 'U')
-   drop table PERFIL
+   drop table PRIVADO.PERFIL
 go
 
 if exists (select 1
             from  sysobjects
-           where  id = object_id('PERFILROL')
+           where  id = object_id('PRIVADO.PERFILROL')
             and   type = 'U')
-   drop table PERFILROL
+   drop table PRIVADO.PERFILROL
 go
 
 if exists (select 1
             from  sysobjects
-           where  id = object_id('PLANCONTRALAFT')
+           where  id = object_id('PRIVADO.PLANCONTRALAFT')
             and   type = 'U')
-   drop table PLANCONTRALAFT
+   drop table PRIVADO.PLANCONTRALAFT
 go
 
 if exists (select 1
             from  sysobjects
-           where  id = object_id('ROL')
+           where  id = object_id('PRIVADO.ROL')
             and   type = 'U')
-   drop table ROL
+   drop table PRIVADO.ROL
 go
 
 if exists (select 1
             from  sysobjects
-           where  id = object_id('USUARIO')
+           where  id = object_id('PRIVADO.USUARIO')
             and   type = 'U')
-   drop table USUARIO
+   drop table PRIVADO.USUARIO
+go
+
+drop schema PRIVADO
+go
+
+/*==============================================================*/
+/* User: PRIVADO                                                */
+/*==============================================================*/
+create schema PRIVADO
 go
 
 /*==============================================================*/
 /* Table: ACCION                                                */
 /*==============================================================*/
-create table ACCION (
+create table PRIVADO.ACCION (
    N_COD_ACCION         int                  identity(1,1),
    N_COD_PLAN           int                  null,
    C_NUM_CODIGO         varchar(50)          null,
@@ -168,7 +177,7 @@ go
 /*==============================================================*/
 /* Table: ENTIDAD                                               */
 /*==============================================================*/
-create table ENTIDAD (
+create table PRIVADO.ENTIDAD (
    N_COD_ENTIDAD        int                  identity(1,1),
    C_DES_RAZON          varchar(200)         null,
    C_COD_RUC            varchar(50)          null,
@@ -186,7 +195,7 @@ go
 /*==============================================================*/
 /* Table: INDICADOR                                             */
 /*==============================================================*/
-create table INDICADOR (
+create table PRIVADO.INDICADOR (
    N_COD_INDICADOR      int                  identity(1,1),
    N_COD_ACCION         int                  null,
    C_DES_NOMBRE         varchar(1000)        null,
@@ -205,7 +214,7 @@ go
 /*==============================================================*/
 /* Table: INDICADORENTIDAD                                      */
 /*==============================================================*/
-create table INDICADORENTIDAD (
+create table PRIVADO.INDICADORENTIDAD (
    N_COD_INDICADOR      int                  identity(1,1),
    N_COD_ENTIDAD        int                  null,
    N_COD_ESTADO         int                  null,
@@ -217,7 +226,7 @@ go
 /*==============================================================*/
 /* Table: MENU                                                  */
 /*==============================================================*/
-create table MENU (
+create table PRIVADO.MENU (
    N_COD_MENU           int                  identity(1,1),
    N_COD_PADRE          int                  null,
    C_DES_NOMBRE         varchar(50)          null,
@@ -233,7 +242,7 @@ go
 /*==============================================================*/
 /* Table: MENUROL                                               */
 /*==============================================================*/
-create table MENUROL (
+create table PRIVADO.MENUROL (
    N_COD_MENU           int                  null,
    N_COD_ROL            int                  null,
    N_FL_ACTIVO          int                  null
@@ -244,7 +253,7 @@ go
 /*==============================================================*/
 /* Table: PERFIL                                                */
 /*==============================================================*/
-create table PERFIL (
+create table PRIVADO.PERFIL (
    N_COD_PERFIL         int                  identity(1,1),
    C_DET_NOMBRE         varchar(100)         null,
    N_FL_ACTIVO          int                  null,
@@ -262,7 +271,7 @@ go
 /*==============================================================*/
 /* Table: PERFILROL                                             */
 /*==============================================================*/
-create table PERFILROL (
+create table PRIVADO.PERFILROL (
    N_COD_PERFIL         int                  null,
    N_COD_ROL            int                  null,
    N_FL_ACTIVO          int                  null
@@ -273,7 +282,7 @@ go
 /*==============================================================*/
 /* Table: PLANCONTRALAFT                                        */
 /*==============================================================*/
-create table PLANCONTRALAFT (
+create table PRIVADO.PLANCONTRALAFT (
    N_COD_PLAN           int                  identity(1,1),
    C_NOM_PLAN           varchar(300)         null,
    C_DES_DETALLE        varchar(1000)        null,
@@ -291,7 +300,7 @@ go
 /*==============================================================*/
 /* Table: ROL                                                   */
 /*==============================================================*/
-create table ROL (
+create table PRIVADO.ROL (
    N_COD_ROL            int                  identity(1,1),
    C_DES_ROL            varchar(50)          null,
    N_FL_ACTIVO          int                  null,
@@ -309,7 +318,7 @@ go
 /*==============================================================*/
 /* Table: USUARIO                                               */
 /*==============================================================*/
-create table USUARIO (
+create table PRIVADO.USUARIO (
    N_COD_ID             int                  identity(1,1),
    N_COD_PERFIL         int                  null,
    N_COD_ENTIDAD        int                  null,
@@ -329,52 +338,52 @@ create table USUARIO (
 ON [PRIMARY]
 go
 
-alter table ACCION
+alter table PRIVADO.ACCION
    add constraint FK_ACCION_REFERENCE_PLAN foreign key (N_COD_PLAN)
-      references PLANCONTRALAFT (N_COD_PLAN)
+      references PRIVADO.PLANCONTRALAFT (N_COD_PLAN)
 go
 
-alter table INDICADOR
+alter table PRIVADO.INDICADOR
    add constraint FK_INDICADO_REFERENCE_ACCION foreign key (N_COD_ACCION)
-      references ACCION (N_COD_ACCION)
+      references PRIVADO.ACCION (N_COD_ACCION)
 go
 
-alter table INDICADORENTIDAD
+alter table PRIVADO.INDICADORENTIDAD
    add constraint FK_INDICADO_REFERENCE_ENTIDAD foreign key (N_COD_ENTIDAD)
-      references ENTIDAD (N_COD_ENTIDAD)
+      references PRIVADO.ENTIDAD (N_COD_ENTIDAD)
 go
 
-alter table INDICADORENTIDAD
+alter table PRIVADO.INDICADORENTIDAD
    add constraint FK_INDICADO_REFERENCE_INDICADO foreign key (N_COD_INDICADOR)
-      references INDICADOR (N_COD_INDICADOR)
+      references PRIVADO.INDICADOR (N_COD_INDICADOR)
 go
 
-alter table MENUROL
+alter table PRIVADO.MENUROL
    add constraint FK_MENUROL_REFERENCE_MENU foreign key (N_COD_MENU)
-      references MENU (N_COD_MENU)
+      references PRIVADO.MENU (N_COD_MENU)
 go
 
-alter table MENUROL
+alter table PRIVADO.MENUROL
    add constraint FK_MENUROL_REFERENCE_ROL foreign key (N_COD_ROL)
-      references ROL (N_COD_ROL)
+      references PRIVADO.ROL (N_COD_ROL)
 go
 
-alter table PERFILROL
+alter table PRIVADO.PERFILROL
    add constraint FK_PERFILRO_REFERENCE_PERFIL foreign key (N_COD_PERFIL)
-      references PERFIL (N_COD_PERFIL)
+      references PRIVADO.PERFIL (N_COD_PERFIL)
 go
 
-alter table PERFILROL
+alter table PRIVADO.PERFILROL
    add constraint FK_PERFILRO_REFERENCE_ROL foreign key (N_COD_ROL)
-      references ROL (N_COD_ROL)
+      references PRIVADO.ROL (N_COD_ROL)
 go
 
-alter table USUARIO
+alter table PRIVADO.USUARIO
    add constraint FK_USUARIO_REFERENCE_ENTIDAD foreign key (N_COD_ENTIDAD)
-      references ENTIDAD (N_COD_ENTIDAD)
+      references PRIVADO.ENTIDAD (N_COD_ENTIDAD)
 go
 
-alter table USUARIO
+alter table PRIVADO.USUARIO
    add constraint FK_USUARIO_REFERENCE_PERFIL foreign key (N_COD_PERFIL)
-      references PERFIL (N_COD_PERFIL)
+      references PRIVADO.PERFIL (N_COD_PERFIL)
 go
